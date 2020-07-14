f6bad6edfebd99d83ab9e855f32e5eadf1a39904
Micah Mosley <micah.mosley@utexas.edu>
2020-07-14 20:09:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,14 @@
 # Make your shoe class here!
 
 class Shoe 
+  
+  attr_accessor :
+  attr_reader :brand
   def initialize(brand)
     @brand=brand 
   end 
   
-  attr_accessor :
+  
+  
   
 end
